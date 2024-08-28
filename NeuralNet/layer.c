026940d053ce394a96af2c89695b4d177810e8ff
prijall <khadkaprijal@gmail.com>
2024-08-28 21:06:51 +0545

    Layer for NN

---
+++ NeuralNet/layer.c
@@ -0,0 +1,10 @@
+#include "layer.h"
+
+
+layer create_layer(int number_of_neurons)
+{
+    layer lay;
+    lay.num_neu-=1;
+    lay.neu=(struct neuron_t *)malloc(number_of_neurons * sizeof(struct neuron_t));
+    return lay;
+}
---
+++ NeuralNet/layer.h
@@ -0,0 +1,14 @@
+#ifndef LAYER_H
+#define LAYER_H
+#include "neuron.h"
+
+
+typedef struct layer_t
+{
+    int num_neu;
+    struct neuron_t *neu; 
+} layer;
+
+layer create_layer(int num_neurons);
+
+#endif
